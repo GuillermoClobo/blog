811bd2e2cabe0f432987d2d4a7287a07a7c72ac3
Guillermo Cervantes <guillermo.c.lobo@gmail.com>
2021-08-04 09:39:59 -0500

    Cleaning code with before-action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
 
+    before_action :find_article, except: [:new, :create]
+    
     def show
-        @article = Article.find(params[:id])
+        
     end
 
     def new
@@ -14,18 +16,20 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
         @article.update(title: params[:article][:title], content: params[:article][:content])
         redirect_to @article
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         redirect_to root_path
     end
+
+    def find_article
+        @article = Article.find(params[:id])
+    end
+
 end
